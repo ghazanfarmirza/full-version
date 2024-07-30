8cde4139cd6a729af6541e862c82676eddea2889
Ghazanfar Mirza <ghazanfar.mirza97@gmail.com>
2024-07-30 15:11:09 +0500

    mock dataset added for listing

---
+++ src/@fake-db/table/static-data-business-users.js
@@ -0,0 +1,92 @@
+export const rows = [
+  {
+    id: 'bu-001',
+    name: 'Magrow Mpc',
+    business: "Red's Cafe",
+    email: 'magrowmpc@gmail.com',
+    roles: 'Business Admin',
+    creationDate: '20 Mar 2024',
+    status: 1
+  },
+  {
+    id: 'bu-002',
+    name: 'Ben Davies',
+    business: 'Kentucky Fried Chicken',
+    email: 'admin@kentuckyfriedchicken.com',
+    roles: 'Business Admin',
+    creationDate: '21 Mar 2024',
+    status: 1
+  },
+  {
+    id: 'bu-003',
+    name: 'Paul Goner',
+    business: 'Kentucky Fried Chicken',
+    email: 'paul@kentuckyfriedchicken.com',
+    roles: 'Store User',
+    creationDate: '21 Mar 2024',
+    status: 1
+  },
+  {
+    id: 'bu-004',
+    name: 'Jenny Baker',
+    business: 'Cake Face',
+    email: 'jenny@cakeface.com',
+    roles: 'Business Admin',
+    creationDate: '26 Mar 2024',
+    status: 1
+  },
+  {
+    id: 'bu-005',
+    name: 'Mark Baker',
+    business: "Mark's Cakes",
+    email: 'mark@markscakes.com',
+    roles: 'Business Admin',
+    creationDate: '26 Mar 2024',
+    status: 1
+  },
+  {
+    id: 'bu-006',
+    name: 'Jenny Baker',
+    business: "Mark's Cakes",
+    email: 'jenny@markscakes.com',
+    roles: 'Store User',
+    creationDate: '26 Mar 2024',
+    status: 1
+  },
+  {
+    id: 'bu-007',
+    name: 'Peter Davies',
+    business: 'Chicken Little',
+    email: 'peter@chickenlittle.com.au',
+    roles: 'Business Admin',
+    creationDate: '20 Apr 2024',
+    status: 1
+  },
+  {
+    id: 'bu-008',
+    name: 'Ted Stevens',
+    business: 'Chicken Little',
+    email: 'ted@chickenlittle.com',
+    roles: 'Store User',
+    creationDate: '19 Apr 2024',
+    status: 1
+  },
+  {
+    id: 'bu-009',
+    name: 'Steve Burger',
+    business: 'Hamburger World',
+    email: 'steve@hamburgerworld.com',
+    roles: 'Business Admin',
+    creationDate: '01 May 2024',
+    status: 1
+  },
+  {
+    id: 'bu-010',
+    name: 'Vinny Curry',
+    business: 'Thai Time',
+    email: 'admin@thaitime.com',
+    roles: 'Business Admin',
+    creationDate: '06 May 2024',
+    status: 1
+  }
+]
---
+++ src/@fake-db/table/static-data-businesses.js
@@ -0,0 +1,74 @@
+export const businesses = [
+  {
+    id: 'b-002',
+    businessName: "Red's Cafe",
+    email: 'davey@redscafe.com.au',
+    phone: '09970628352',
+    address: 'Dandenong, VIC, 3175',
+    status: 1
+  },
+  {
+    id: 'b-003',
+    businessName: 'Kentucky Fried Chicken',
+    email: 'admin@kentuckyfriedchicken.com',
+    phone: '03 9855 8547',
+    address: 'Melbourne, VIC, 3000',
+    status: 1
+  },
+  {
+    id: 'b-004',
+    businessName: 'Cake Face',
+    email: 'jenny@cakeface.com',
+    phone: '04212336598',
+    address: 'Melbourne, VIC, 3000',
+    status: 1
+  },
+  {
+    id: 'b-005',
+    businessName: "Mark's Cakes",
+    email: 'mark@markscakes.com',
+    phone: '0433333333',
+    address: 'Caulfield, QLD, 3161',
+    status: 1
+  },
+  {
+    id: 'b-006',
+    businessName: 'Chicken Little',
+    email: 'peter@chickenlittle.com.au',
+    phone: '03 9212 1535',
+    address: 'Fitzroy, VIC, 3065',
+    status: 1
+  },
+  {
+    id: 'b-007',
+    businessName: 'Hamburger World',
+    email: 'steve@hamburgerworld.com',
+    phone: '03955595555',
+    address: 'Collingwood, VIC, 3065',
+    status: 1
+  },
+  {
+    id: 'b-008',
+    businessName: 'Thai Time',
+    email: 'admin@thaitime.com',
+    phone: '03 9789 6548',
+    address: 'Hawthorn, VIC, 3122',
+    status: 1
+  },
+  {
+    id: 'b-009',
+    businessName: 'Steaks Ahoy!',
+    email: 'darren@steaksahoy.com',
+    phone: '0396859856',
+    address: 'Frankston, VIC, 3000',
+    status: 1
+  },
+  {
+    id: 'b-010',
+    businessName: 'Vege World',
+    email: 'vege@vegeworld.com',
+    phone: '0395896589',
+    address: 'Richmond, VIC, 3000',
+    status: 1
+  }
+]
